9539e154f05538f41f9cedde276578546473438a
Peace <macio-111@o2.pl>
2017-04-13 13:38:16 +0200

    First push

---
+++ GraphicsMonteCarloPI/Form.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace GraphicsMonteCarloPI
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tnrAppTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tnrAppTimer
+            // 
+            this.tnrAppTimer.Enabled = true;
+            this.tnrAppTimer.Interval = 10;
+            this.tnrAppTimer.Tick += new System.EventHandler(this.tnrAppTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 589);
+            this.Name = "Form1";
+            this.Text = "PIMonteCarlo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tnrAppTimer;
+    }
+}
+
---
+++ GraphicsMonteCarloPI/Form.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace GraphicsMonteCarloPI
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        Random rnd = new Random();
+        List<KeyValuePair<float, float>> list = new List<KeyValuePair<float, float>>();
+
+        public Form()
+        {
+            InitializeComponent();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {       
+       
+            this.DoubleBuffered = true;
+
+            Paint += new PaintEventHandler(Form1_Paint);
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen ellipsePen = new Pen(Color.Red, 2); //pen
+
+            e.Graphics.DrawRectangle(Pens.Black, new Rectangle(0, 0, 200, 200));
+            e.Graphics.DrawEllipse(ellipsePen, new Rectangle(0, 0, 200, 200));
+
+            double piApproximation = 0;
+            int total = 0;
+            int numInCircle = 0;
+            float x, y; // Coordinates of the random point.
+            float xG, yG; // Graphics scale
+            int iterations = 0;
+            int i = 0;
+
+            // Generate random points until our approximation within
+            // the desired tolerance.
+            //while (Math.Abs(Math.PI - piApproximation) > tolerance)
+            Console.WriteLine("How many iterations?");
+
+            iterations = Convert.ToInt32(Console.ReadLine());
+
+            while (i < iterations)
+            {
+                x = rnd.Next(-100, 100);
+                y = rnd.Next(-100, 100);
+
+                xG = x + 100;
+                yG = y + 100;
+
+                //    e.Graphics.DrawRectangle(Pens.Black, new Rectangle((int)xG, (int)yG, 1, 1));
+                /*
+                    list.Add(new KeyValuePair<float, float>(xG, yG));
+
+                    foreach(KeyValuePair<float, float> element in list)
+                        e.Graphics.DrawRectangle(Pens.Black, new Rectangle((int)element.Value, (int)element.Key, 1, 1));
+                */
+                //e.Graphics.DrawRectangle(Pens.Black, new Rectangle((int)xG, (int)yG, 1, 1));
+
+                e.Graphics.FillRectangle(Brushes.Black, xG, yG, 1, 1); //drawing pixel          
+
+                x = x / 100;
+                y = y / 100;
+
+                if (x * x + y * y <= 1.0) // Is the point in the circle?
+                {
+                    ++numInCircle;
+                }
+                ++total;
+
+                i++;
+            
+            }
+
+            piApproximation = 4.0 * ((double)numInCircle / (double)total);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Pi calculated with {0} random points.", total);
+            Console.WriteLine("Approximated Pi = {0}", piApproximation);
+         
+        }
+
+        private void tnrAppTimer_Tick(object sender, EventArgs e)
+        {
+           //this.Refresh();
+        }
+    }
+}
